f166f75225f0247d785f10e33edaf76c910ef432
reynaldo0 <reynaldooyusellino@gmail.com>
2025-02-12 15:32:14 +0700

    add : swiper kasus and data kasus section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar"
+import About from "./pages/About"
 import Home from "./pages/Home"
+import Kasus from "./pages/Kasus"
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-
+      <About />
+      <Kasus />
     </>
   )
 }
